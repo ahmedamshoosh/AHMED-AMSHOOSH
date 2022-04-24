8cb25ee0fc2e2f8872bc15d695f884297a1958c3
ahmedamshoosh <103686519+ahmedamshoosh@users.noreply.github.com>
2022-04-25 01:28:30 +0300

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,11 @@
+function openfunction(){
+
+    document.getElementById('list').style.width="200px";
+}
+
+
+function closefunction(){
+
+    document.getElementById('list').style.width="0px";
+
+}
